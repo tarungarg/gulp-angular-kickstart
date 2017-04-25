21a17a73a420f16b6837b0c52b3e04b84ba639ba
Tarun <t.garg28@gmail.com>
2017-04-25 12:41:28 +0530

    first commit

---
+++ dist/js/main.js
@@ -0,0 +1,1 @@
+!function(){"use strict";angular.module("SampleApp",["ngRoute","ngAnimate"]).config(["$locationProvider","$routeProvider",function(e,o){e.hashPrefix("!"),o.when("/",{templateUrl:"./partials/partial1.html",controller:"MainController"}).otherwise({redirectTo:"/"})}]),angular.module("SampleApp").controller("MainController",["$scope",function(e){e.test="Testing..."}])}();
